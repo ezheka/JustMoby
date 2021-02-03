e9ccb0094555df7cba1a7d8b3b41d56a6d277a2b
ezheka <60533956+ezheka@users.noreply.github.com>
2021-02-03 20:59:46 +0500

    add Task 2

---
+++ Task 2/Unity Project/Assets/Scripts/AdaptationJoystick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdaptationJoystick : MonoBehaviour
+{
+    public RectTransform joystick;
+    public RectTransform handheld;
+
+
+    private int _size;
+    void Start()
+    {
+        _size = Camera.main.pixelWidth / 10;
+
+        joystick.sizeDelta = new Vector2(_size * 2, _size * 2);
+        handheld.sizeDelta = new Vector2(_size, _size);
+    }
+
+}
